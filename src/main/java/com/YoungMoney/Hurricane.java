8871ead2f1c48b9617166d6152f9a9f392bb2d54
Steven Burris <stevenburris.jr@gmail.com>
2016-10-21 11:32:49 -0400

    Added Hurricane entity;

--- src/main/java/com/YoungMoney/Hurricane.java
+++ src/main/java/com/YoungMoney/Hurricane.java
@@ -1,7 +1,81 @@
 package com.YoungMoney;
 
+import javax.persistence.*;
+
 /**
  * Created by stevenburris on 10/21/16.
  */
+@Entity
+@Table(name = "hurricanes")
 public class Hurricane {
+    enum Category {
+        ONE, TWO, THREE, FOUR, FIVE
+    }
+
+    @Id
+    @GeneratedValue
+    int id;
+
+    @Column(nullable = false)
+    String name;
+
+    @Column(nullable = false)
+    String location;
+
+    @Column(nullable = false)
+    Category category;
+
+    @Column(nullable = false)
+    String image;
+
+    public Hurricane() {
+    }
+
+    public Hurricane(int id, String name, String location, Category category, String image) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.category = category;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
