91bb25d422ae65b38496b5b28057e703293c24c3
Steven Burris <stevenburris.jr@gmail.com>
2016-10-24 10:23:24 -0400

    Added filtering and searching;

--- src/main/java/com/YoungMoney/HurricaneRepo.java
+++ src/main/java/com/YoungMoney/HurricaneRepo.java
@@ -2,8 +2,17 @@ package com.YoungMoney;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by stevenburris on 10/21/16.
  */
 public interface HurricaneRepo extends CrudRepository<Hurricane, Integer> {
+    List<Hurricane> findByCategory(Hurricane.Category category);
+    List<Hurricane> findByLocation(String location);
+    List<Hurricane> findByName(String name);
+    List<Hurricane> findByNameContainingIgnoreCaseOrLocationContainingIgnoreCase(String name, String location);
+
+    Hurricane findFirstByLocation(String location);
+    int countByCategory(Hurricane.Category category);
 }
--- src/main/java/com/YoungMoney/HurricaneTrackerController.java
+++ src/main/java/com/YoungMoney/HurricaneTrackerController.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
 /**
  * Created by stevenburris on 10/21/16.
  */
@@ -14,10 +16,21 @@ public class HurricaneTrackerController {
     @Autowired
     HurricaneRepo hurricanes;
 
+
+    //home page,
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String home(Model model) {
-        Iterable<Hurricane> hlist = hurricanes.findAll();
-        model.addAttribute("hurricanes", hlist);
+    public String home(Model model, Hurricane.Category category, String search) {
+        List<Hurricane> hList;
+        if (category != null) {
+            hList = hurricanes.findByCategory(category);
+        }
+        else if(search != null) {
+            hList = hurricanes.findByNameContainingIgnoreCaseOrLocationContainingIgnoreCase(search,search);
+        }
+        else {
+            hList = (List<Hurricane>) hurricanes.findAll();
+        }
+        model.addAttribute("hurricanes", hList);
         return "home";
     }
 
@@ -28,7 +41,7 @@ public class HurricaneTrackerController {
         return "redirect:/";
     }
 
-
+//    @RequestMapping(path = /category)
 
 //    log in route
 //    @RequestMapping(path = "/login", method = RequestMethod.POST)
